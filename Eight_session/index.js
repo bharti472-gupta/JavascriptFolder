8e50e7ebe03cd187d345d4ef1d86e13bb07f8f3b
Bharti Gupta <bhartiguptaxo@gmail.com>
2026-04-06 14:01:06 +0530

    2 more files saved

---
+++ Eight_session/index.js
@@ -0,0 +1,33 @@
+//PRACTICE SET - BITWISE AND TERNARY OPERATOR
+
+//BITWISE OPERATOR
+
+//Q1.
+// console.log(5 & 3);
+
+//Q2.
+// console.log(4 | 2);
+
+//Q3.
+// console.log(4 ^ 7);
+
+//TERNARY OPERATOR
+
+//Q1
+//  let num = 7;
+//  (num % 2 == 0)? console.log("Even Number"): console.log("Odd Number");
+
+// let num1 = 8;
+// (num1 % 2 == 0)? console.log("Even Number"): console.log("Odd Number");
+
+
+//Q2.
+// let a = 23;
+// let b = 45;
+// (a < b)? console.log("Maximum No is b = ",b) : console.log(" Maximum No is a = ",a);
+
+// let a1 = 56;
+// let b1 = 45;
+// (a1 < b1)? console.log("Maximum No is b1 = ",b1) : console.log(" Maximum No is a1 = ",a1);
+
+
---
+++ Ninth_Session/index.js
@@ -0,0 +1,167 @@
+//CONDITIONALS - IF ELSE STATEMENTS
+
+//Q1.
+
+// let num = 5;
+// if(num % 2 == 0){
+//     console.log("Even Number");
+    
+// }
+// else{
+//     console.log("Odd Number");
+    
+// }
+
+
+//Q2.
+
+// let num = -5;
+// if(num >= 0){
+//     console.log("Positive Number");
+    
+// }
+// else{
+//     console.log("Negative Number");
+    
+// }
+
+//Q3.
+
+// let age = 16;
+// if(age >= 18){
+//     console.log(" Eligible to vote");
+    
+// }
+// else{
+//     console.log("Not eligible to vote");
+    
+// }
+
+//Q4.
+
+// let num = 40;
+//  if(num % 10 == 0){
+//      console.log(" divisible by 10 ");
+    
+//  }
+//  else{
+//      console.log(" Not divisible by 10 ");
+    
+//  }
+
+
+//Q5.
+
+//  let marks = 33;
+//  if(marks >= 40 ){
+//      console.log(" pass ");
+    
+//  }
+//  else{
+//      console.log(" fail ");
+    
+//  }
+
+//Q6.
+
+// let num1 = 18;
+// let num2 = 25;
+//  if(num1 <= num2){
+//     console.log( num2 + " is greater");
+    
+//  }
+//  else{
+//      console.log(num1 + "  is greater");
+    
+//  }
+
+//Q7.
+
+// let num = 145;
+// if(num >= 100 && num <= 999){
+//     console.log("3-digit number");
+    
+// }
+// else{
+//     console.log("Not a 3-digit number");
+    
+// }
+
+
+//Q8.
+// let CP = 400;
+// let SP = 450;
+// if(SP > CP){
+
+//     console.log("Profit = ",SP - CP);
+  
+// }
+// else{
+
+//     console.log("Loss = ",CP -SP);
+    
+// }
+
+
+// let CP = 400;
+//  let SP = 40;
+//  if(SP > CP){
+//      console.log("Profit = ",SP - CP);
+//  }
+//  else if(SP == CP){
+//  console.log("No Profit No loss");
+//  }
+//  else{
+//     console.log("Loss = ",CP -SP);
+//  }
+
+//Q9.
+// let a = 60;
+// let b = 60;
+// let c = 60;
+// if( a+b+c == 180){
+// console.log("Valid Triangle");
+
+// }
+// else{
+//     console.log(" Not a Valid Triangle");
+    
+// }
+
+//Q10.
+// let age = 19;
+// if(age >= 18){
+//     console.log("Eligible for driving license");
+    
+// }
+// else{
+//     console.log(" Not Eligible for driving license"); 
+// }
+
+//Q11.
+
+// let a = 10;
+// let b = "10";
+// if(a === b){
+//     console.log("Equal");
+    
+// }
+// else{
+//     console.log(" Not Equal");
+// }
+
+//Q12.
+
+
+// let a = 5;
+// let b = 10;
+// if(a !== b){
+//     console.log("  Not Equal");
+    
+// }
+// else{
+//     console.log("  Equal");
+// }
+
+// In (!==) condition even if one condition is true then the result will be true 
+// In (===) both condition is true then the result will be true
